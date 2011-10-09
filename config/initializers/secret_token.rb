b0597565f05b49427249c8f4b567f3635f47ce4a
Fabio Trezzi <fabio.trezzi@gmail.com>
2011-10-09 21:38:38 +0200

    Applicazione rails vergine
    Signed-off-by: Fabio Trezzi <fabio.trezzi@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cad19::Application.config.secret_token = '28c3ab041726c245911e41a51e5b9b0217e1786b1470750d4affb8b73e1619ea56fac6020c46b7a3c8df6caa0586ac3223e9874fe26126ea78736d26fdfc0e1e'
